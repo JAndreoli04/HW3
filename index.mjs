fdda32ae300773c2f77f497a4b0be6f4e85d7b75
Justin <justin.andreoli04@gmail.com>
2026-04-05 20:23:21 -0700

    Completed hw3 requirements

--- index.mjs
+++ index.mjs
@@ -5,6 +5,7 @@ import express from 'express';
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+import { DateTime } from 'luxon';
 
 
 app.get('/', (req, res) => {
@@ -20,19 +21,21 @@ app.get('/searchLocation', async (req, res) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&limit=1&units=imperial&appid=836a84a6abc77828051d5add0a2f27ef`
     let response = await fetch(url);
     let data = await response.json();
+    const readableTime = DateTime.fromSeconds(data.dt)
+                           .toLocaleString(DateTime.DATETIME_MED);
 
-    // console.log(data);
-
-    res.render('results.ejs', {location, data})
+    res.render('results.ejs', {location, data, readableTime})
 });
 
 app.get('/current', async(req, res) => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=Monterey&limit=1&units=imperial&appid=836a84a6abc77828051d5add0a2f27ef`
     let response = await fetch(url);
     let data = await response.json();
+    const readableTime = DateTime.fromSeconds(data.dt)
+                           .toLocaleString(DateTime.DATETIME_MED);
 
     
-   res.render('current.ejs', {data});
+   res.render('current.ejs', {data, readableTime});
 });
 
 
